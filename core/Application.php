ab76a892af7d95fc622e4bc3e25486e0cea58c5d
He2Henrique <134214073+He2Henrique@users.noreply.github.com>
2025-08-22 16:56:12 -0300

    making a framework?

---
+++ core/Application.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\core;
+
+class Application{
+
+    public Router $router;
+    public function __construct()
+    {
+        
+    }
+}
---
+++ core/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\core;
+
+class Router{
+
+    public function __construct()
+    {
+        
+    }
+}
